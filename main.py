c2f34702d97597fa1c98f309878c68d20320da4c
Michael Ludvig <mludvig@logix.net.nz>
2022-11-25 12:16:02 +1300

    Better support for retries and error tiles

--- main.py
+++ main.py
@@ -14,38 +14,33 @@ from PIL import Image, ImageDraw, ImageFont
 from display import Display
 from touch import TouchScreen
 
-BUZZER_PIN=2
+BUZZER_PIN=17
 
 touch_queue = queue.Queue(1)
 
 async def fetch_one_url(session, url, url_id, headers):
-    print(f"{url=}")
-    async with session.get(url, headers=headers) as resp:
-        return {
-            "id": url_id,
-            "status": resp.status,
-            "content": await resp.read(),
-        }
+    tries = 5
+    while tries:
+        try:
+            async with session.get(url, headers=headers) as resp:
+                if resp.ok:
+                    return {
+                        "id": url_id,
+                        "content": await resp.read(),
+                    }
+                print(f"ERROR {url} : status={resp.status} ({resp.reason})")
+        except Exception as ex:
+            print(f"ERROR {url} : {ex}")
+        tries -= 1
+        await asyncio.sleep(0.5)
+    return {
+        "id": url_id,
+        "content": None,
+    }
 
 async def fetch_urls(urls, token):
     """
     Fetch the "urls" and return the data.
-
-    Parameters:
-        urls: dict of dicts
-            {
-                "id1": { "url": "http://..." },
-                "id2": { "url": "http://..." },
-            }
-        token: str
-            Grafana service account token, e.g. "gsat_xyzabcd"
-
-    Returns:
-        dict of dicts:
-            {
-                "id1": { "status": 200, "content": ... },
-                "id2": { "status": 500, "content": None },
-            }
     """
 
     headers = {
@@ -62,20 +57,25 @@ async def fetch_urls(urls, token):
             tasks.append(asyncio.ensure_future(fetch_one_url(session, url, url_id, headers)))
         data_raw = await asyncio.gather(*tasks)
 
-    print(f"Load time: {time.time()-ts_start}")
-
     # Convert to dict with 'id' as the key
     data = { item["id"]: item for item in data_raw }
 
     return data
 
 
-def build_wait_screen(width, height):
-    dashboard_image = Image.new("RGB", (width, height), "#000")
+def wait_screen(disp, message = "Please wait..."):
+    dashboard_image = Image.new("RGB", (disp.d_width, disp.d_height), "#000")
     draw = ImageDraw.Draw(dashboard_image)
-    font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 40)
-    draw.text((10, 10), "Please wait ...", font=font, fill="#FF0")
-    return dashboard_image
+    font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 32)
+    draw.text((10, disp.d_height//3), message, font=font, fill="#FF0")
+    disp.display_image(dashboard_image)
+
+def error_tile():
+    error_image = Image.new("RGB", (30, 30), "#FFF")
+    draw = ImageDraw.Draw(error_image)
+    font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 30)
+    draw.text((1, 1), "X", font=font, fill="#A00")
+    return error_image
 
 class DashboardManager:
     def __init__(self, config, disp):
@@ -96,16 +96,18 @@ class DashboardManager:
                 "id": tile["id"],
                 "url": tile["url"]
             })
+        #print("Starting to download URLs...")
         data = asyncio.run(fetch_urls(urls, self.config['general']['grafana_token']))
 
         dashboard_image = Image.new("RGB", (self.disp.d_width, self.disp.d_height), "#FFF")
         for tile in self.current_dashboard["tiles"]:
             try:
                 image = Image.open(io.BytesIO(data[tile["id"]]["content"]))
-                dashboard_image.paste(image, tile.get("placement", (0,0)))
             except Exception as ex:
                 print(f"ERROR: {ex}")
+                image = error_tile()
                 # Never mind, move on...
+            dashboard_image.paste(image, tile.get("placement", (0,0)))
 
         self.disp.display_image(dashboard_image)
         return self.current_dashboard.get("touch_areas", None)
@@ -132,20 +134,30 @@ def main():
         config = yaml.safe_load(config_yaml)
 
     disp = Display(rotation=config["general"]["rotation"])
-    disp.display_image(build_wait_screen(disp.d_width, disp.d_height))
+    wait_screen(disp)
+
     dash_manager = DashboardManager(config, disp)
 
     touch_screen = TouchScreen(touch_queue, BUZZER_PIN, config["general"]["rotation"])
 
-    dash_name = "main"
+    dash_change_timestamp = time.time()
+    dash_revert_timeout = 60*5      # seconds
+
+    dash_name_default = "main"
+    dash_name = dash_name_default
     while True:
+        if time.time() - dash_change_timestamp > dash_revert_timeout:
+            dash_name = dash_name_default
+            dash_change_timestamp = time.time()
         touch_areas = dash_manager.show_dashboard(dash_name)
         try:
             touch_coords = touch_queue.get(timeout=5)
         except queue.Empty:
             continue
-        new_dash = dash_manager.resolve_touch(touch_coords)
-        print(f"Switching to dashboard: {new_dash}")
-        if new_dash is not None:
-            dash_name = new_dash
-
+        new_dash_name = dash_manager.resolve_touch(touch_coords)
+        print(f"Switching to dashboard: {new_dash_name}")
+        if new_dash_name is not None:
+            dash_name = new_dash_name
+            dash = dash_manager.find_dashboard(dash_name)
+            wait_screen(disp, message=f"Loading dashboard:\n{dash.get('label', dash_name)}")
+            dash_change_timestamp = time.time()
